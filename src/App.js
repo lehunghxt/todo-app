71731728cd4b5daacb5f52aa463528359d12345b
Hung Le <leehxt@gmail.com>
2020-11-03 21:47:19 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, HashRouter } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
@@ -56,7 +56,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <HashRouter>
         <div className="App">
           <div className="container">
             <Header />
@@ -77,7 +77,7 @@ class App extends Component {
             <Route path="/about" component={About} />
           </div>
         </div>
-      </Router>
+      </HashRouter>
     );
   }
 }
